99737145b47f7af35dea2120b77cc247ab1bc257
matthew <matthewkenigsberg@gmail.com>
2019-04-22 12:49:50 -0500

    renamed

---
+++ db/settings.py
@@ -0,0 +1,6 @@
+path="data/millionsongsubset_full/MillionSongSubset/data"
+user="root"
+password="INGlkYT8jn1pEWPP3o$ANvO4O70eaH!3OClDBi3BtDczG$$%$xnr3rM&7B1wU5csiA1LBo!PyAF%^7&gbQu1rTP8PUGR8alJEZmc"
+host="localhost"
+database="msd2"
+max=100
